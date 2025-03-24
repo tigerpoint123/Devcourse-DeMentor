b2984901ee00a77d123f02a0b455a4fb4b476a6b
Daewook123 <cdo5080@gmail.com>
2025-03-24 15:57:30 +0900

    create

---
+++ src/main/java/com/dementor/DeMentorApplication.java
@@ -0,0 +1,13 @@
+package com.dementor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeMentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dementor/DeMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dementor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeMentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
