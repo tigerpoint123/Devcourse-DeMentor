a504c961dc62c0b8312ce10479468741d0e80d38
davinyakma <poohcdv3652@naver.com>
2025-03-26 14:27:18 +0900

    Refactor: MentorResponseDto 수정

--- src/main/java/com/dementor/domain/mentor/dto/MentorResponseDto.java
+++ src/main/java/com/dementor/domain/mentor/dto/MentorResponseDto.java
@@ -7,37 +7,34 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class MentorResponseDto {
-    public static class Response {
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Basic {
+        private Long mentorId;
+        private Long jobId;
+        private Long userId;
+        private String name;
+        private Integer career;
+        private String phone;
+        private String introduction;
+        private Boolean isApproved;
+        private Boolean isModified;
 
-        @Getter
-        @NoArgsConstructor
-        @AllArgsConstructor
-        @Builder
-        public static class Basic {
-            private Long mentorId;
-            private Long jobId;
-            private Long userId;
-            private String name;
-            private Integer career;
-            private String phone;
-            private String introduction;
-            private Boolean isApproved;
-            private Boolean isModified;
-
-            // Entity를 DTO로 변환하는 정적 메서드
-            public static Basic from(MentorEntity entity) {
-                return Basic.builder()
-                        .mentorId(entity.getMentorId())
-                        .jobId(entity.getJobId())
-                        .userId(entity.getUserId())
-                        .name(entity.getName())
-                        .career(entity.getCareer())
-                        .phone(entity.getPhone())
-                        .introduction(entity.getIntroduction())
-                        .isApproved(entity.getIsApproved())
-                        .isModified(entity.getIsModified())
-                        .build();
-            }
+        // Entity를 DTO로 변환하는 정적 메서드
+        public static Basic from(MentorEntity entity) {
+            return Basic.builder()
+                    .mentorId(entity.getMentorId())
+                    .jobId(entity.getJobId())
+                    .userId(entity.getUserId())
+                    .name(entity.getName())
+                    .career(entity.getCareer())
+                    .phone(entity.getPhone())
+                    .introduction(entity.getIntroduction())
+                    .isApproved(entity.getIsApproved())
+                    .isModified(entity.getIsModified())
+                    .build();
         }
     }
 }
