62eae1eb1ac3503514b541da461f8ec17b9a50e2
Daewook123 <cdo5080@gmail.com>
2025-03-26 16:17:57 +0900

    feat: Member 엔티티 추가

---
+++ src/main/java/com/dementor/domain/member/entity/Member.java
@@ -0,0 +1,44 @@
+package com.dementor.domain.member.entity;
+
+import com.dementor.domain.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Entity
+@Builder
+@AllArgsConstructor
+@Getter
+public class Member extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Email
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false, unique = true)
+	private String nickname;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private UserRole role;
+
+	public Member() {
+	}
+
+}
+
+
---
+++ src/main/java/com/dementor/domain/member/entity/UserRole.java
@@ -0,0 +1,6 @@
+package com.dementor.domain.member.entity;
+
+public enum UserRole {
+	MENTOR, //멘토 이자 멘티
+	MENTEE // 멘티
+}
