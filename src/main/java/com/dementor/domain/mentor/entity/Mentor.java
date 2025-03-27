9411a3f3acc3a5bd1a8f2fe89650931b06ff4eee
Daewook123 <cdo5080@gmail.com>
2025-03-27 13:31:15 +0900

    fix : category->job 으로 변경, mentor 엔티티 수정

--- src/main/java/com/dementor/domain/mentor/entity/Mentor.java
+++ src/main/java/com/dementor/domain/mentor/entity/Mentor.java
@@ -1,70 +1,83 @@
 package com.dementor.domain.mentor.entity;
 
-import com.dementor.domain.categories.entity.Categories;
+import java.util.List;
+
+import com.dementor.domain.categories.entity.Job;
 import com.dementor.domain.member.entity.Member;
 import com.dementor.domain.mentoringclass.entity.MentoringClass;
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
+import lombok.Setter;
 
 @Entity
 @Table(name = "mentor")
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder(toBuilder = true)
+@Builder
 public class Mentor {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "mentor_id", nullable = false)
-    private Long mentorId;
+    @Column(name = "member_id")
+    private Long id;
 
     // Member 엔티티와의 관계 (일대일)
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
+    @MapsId // member_id를 PK이자 FK로 사용
+    @JoinColumn(name = "member_id")
     private Member member;
 
     // categories 엔티티와의 관계 (다대일)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "job_id", nullable = false)
-    private Categories category;
+    private Job job;
 
     // PostAttachment 엔티티와의 관계 (일대일, 식별관계)
     //todo: 파일첨부 테이블과 연관관계
     //private PostAttachment attachment;
 
     // Mentoring 수업 엔티티와의 관계 (일대다)
-    @Builder.Default
     @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "mentor_id")
-    private List<MentoringClass> mentorings = new ArrayList<>();
+    @JoinColumn(name = "mentoringClass_id")
+    private List<MentoringClass> mentorings;
 
-    @Column(name = "name", length = 10, nullable = false)
+    @Column(length = 10, nullable = false)
     private String name;
 
-    @Column(name = "current_company", length = 20)
+    @Column(length = 20)
     private String currentCompany;
 
-    @Column(name = "career", nullable = false)
+    @Column(nullable = false)
     private Integer career;
 
-    @Column(name = "phone", length = 20, nullable = false)
+    @Column(length = 20, nullable = false)
     private String phone;
 
-    @Column(name = "introduction", length = 255, nullable = false)
+    @Column(length = 255, nullable = false)
     private String introduction;
 
+    @Setter
     @Enumerated(EnumType.STRING)
-    @Column(name = "is_approved", nullable = false)
+    @Column(nullable = false)
     private ApprovalStatus isApproved;
 
+    @Setter
     @Enumerated(EnumType.STRING)
-    @Column(name = "is_modified", nullable = false)
+    @Column(nullable = false)
     private ApprovalStatus isModified;
 
     @Column(name = "best_for", length = 255)
@@ -73,29 +86,14 @@ public class Mentor {
     @Column(name = "attachment_id")
     private Long attachmentId;
 
-    // ApprovalStatus Enum 정의
+	public Mentor() {
+	}
+
+	// ApprovalStatus Enum 정의
     public enum ApprovalStatus {
         Y, N
     }
 
-    public Mentor updateInfo(Categories category, String name, Integer career,
-                             String phone, String introduction) {
-        return this.toBuilder()
-                .category(category != null ? category : this.category)
-                .name(name != null ? name : this.name)
-                .career(career != null ? career : this.career)
-                .phone(phone != null ? phone : this.phone)
-                .introduction(introduction != null ? introduction : this.introduction)
-                .isModified(ApprovalStatus.Y)
-                .build();
-    }
-
-    // 승인 상태 변경 메서드
-    public Mentor updateApproval(ApprovalStatus isApproved) {
-        return this.toBuilder()
-                .isApproved(isApproved)
-                .build();
-    }
 
     // 멘토링 수업 추가 메서드
     public void addMentoringClass(MentoringClass mentoringClass) {
