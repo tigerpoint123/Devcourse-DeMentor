2f5e582601a8d74f88844f6f8e3de45b88c09c23
Daewook123 <cdo5080@gmail.com>
2025-03-31 10:52:40 +0900

    테스트 코드 수정

--- src/test/java/com/dementor/member/controller/MemberControllerTest.java
+++ src/test/java/com/dementor/member/controller/MemberControllerTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
@@ -40,9 +39,6 @@ public class MemberControllerTest {
 
 	@Autowired
 	private ObjectMapper objectMapper;
-
-	@Autowired
-	private StringRedisTemplate redisTemplate;
 
 	@MockitoBean
 	private MemberRepository memberRepository;
