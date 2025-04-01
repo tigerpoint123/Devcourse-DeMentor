27c0f6e7950b5e7b6cdde2b6bbd42b5cfb8556cf
pingu0118 <rnjsqhrud99@naver.com>
2025-04-01 11:08:05 +0900

    refactor: service 분리에 따른 ìcontroller 수정

--- src/main/java/com/dementor/domain/chat/controller/WebSocketController.java
+++ src/main/java/com/dementor/domain/chat/controller/WebSocketController.java
@@ -2,6 +2,7 @@ package com.dementor.domain.chat.controller;
 
 import com.dementor.domain.chat.dto.ChatMessageResponseDto;
 import com.dementor.domain.chat.dto.ChatMessageSendDto;
+import com.dementor.domain.chat.service.ChatMessageService;
 import com.dementor.domain.chat.service.ChatRoomService;
 import com.dementor.global.jwt.JwtParser;
 import lombok.RequiredArgsConstructor;
@@ -15,24 +16,25 @@ import org.springframework.stereotype.Controller;
 public class WebSocketController {
 
     private final ChatRoomService chatRoomService;
+    private final ChatMessageService chatMessageService;
     private final SimpMessageSendingOperations messagingTemplate;
     private final JwtParser jwtParser;
 
     @MessageMapping("/chat/message")
     public void sendMessage(ChatMessageSendDto dto, @Header("Authorization") String token) {
         try {
-            Long memberId = jwtParser.getMemberId(token); //✅
+            Long memberId = jwtParser.getMemberId(token);
             String nickname = jwtParser.getNickname(token);
 
-            ChatMessageResponseDto response = chatRoomService.handleMessage(dto, memberId, nickname);
+            ChatMessageResponseDto response = chatMessageService.handleMessage(dto, memberId, nickname);
             messagingTemplate.convertAndSend("/sub/chat/room/" + dto.getApplymentId(), response);
         }
-        // 웹소켓 연결시  예외처리(JWT 인증 실패 등의 예외가 발생해도 서버가 죽지 않도록)
         catch (Exception e) {
             System.err.println("WebSocket 인증 오류: " + e.getMessage());
         }
     }
 }
+
 
 
 // -메시지 보낼 때 예외처리 (JWT가 잘못되었을 경우 예외처리)
