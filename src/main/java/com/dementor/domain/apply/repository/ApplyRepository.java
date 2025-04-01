05da02ad81de501b668d05f394fbf81a819c0842
baekminjin <bhs3392@naver.com>
2025-04-01 14:36:02 +0900

    fix: 필요없는 repository 삭제

--- src/main/java/com/dementor/domain/apply/repository/ApplyRepository.java
+++ src/main/java/com/dementor/domain/apply/repository/ApplyRepository.java
@@ -1,12 +1,9 @@
 package com.dementor.domain.apply.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dementor.domain.apply.entity.Apply;
 
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
 
-	Optional<Apply> findByMentoringClassIdAndMemberId(Long classId, Long memberId);
 }
