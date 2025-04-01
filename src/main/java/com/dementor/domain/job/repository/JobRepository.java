0ebd9bc344f2c18900a7b35908a7c9ca08c3746f
Honam Kim <tigerrla@naver.com>
2025-04-01 10:29:12 +0900

    feat : test 코드 수정

--- src/main/java/com/dementor/domain/job/repository/JobRepository.java
+++ src/main/java/com/dementor/domain/job/repository/JobRepository.java
@@ -1,1 +1,7 @@
- 
+package com.dementor.domain.job.repository;
+
+import com.dementor.domain.job.entity.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+} 
