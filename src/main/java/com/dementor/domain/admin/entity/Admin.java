79db8eefdec9088fb6ed2e8e0be0533a3f17735e
pingu0118 <rnjsqhrud99@naver.com>
2025-04-03 18:16:40 +0900

    refactor: for merge

--- src/main/java/com/dementor/domain/admin/entity/Admin.java
+++ src/main/java/com/dementor/domain/admin/entity/Admin.java
@@ -11,17 +11,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import jakarta.persistence.*;
-import com.dementor.global.base.BaseEntity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+
+import lombok.Setter; //챗 부분 필요. ID부분만 붙여도됨
 
 @Setter
 @Entity
