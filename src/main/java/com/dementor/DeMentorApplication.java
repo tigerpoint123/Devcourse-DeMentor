8b5c094de1bbf1d11efff980aced97acd428be9d
Daewook123 <cdo5080@gmail.com>
2025-04-04 11:45:32 +0900

    dev pull rebase

--- src/main/java/com/dementor/DeMentorApplication.java
+++ src/main/java/com/dementor/DeMentorApplication.java
@@ -2,7 +2,9 @@ package com.dementor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 @SpringBootApplication
 public class DeMentorApplication {
 	// 개발 세팅 중
