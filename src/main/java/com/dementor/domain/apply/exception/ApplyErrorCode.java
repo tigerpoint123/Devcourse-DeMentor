c8a22cc5c765eb9cff5d8bac89358c4bf307ad78
baekminjin <bhs3392@naver.com>
2025-04-04 10:06:47 +0900

    feat: apply 에러코드 추가

--- src/main/java/com/dementor/domain/apply/exception/ApplyErrorCode.java
+++ src/main/java/com/dementor/domain/apply/exception/ApplyErrorCode.java
@@ -11,7 +11,8 @@ public enum ApplyErrorCode {
 
 	SCHEDULE_REQUIRED(HttpStatus.BAD_REQUEST, "멘토링 일정을 선택해야 합니다."),
 	APPLY_NOT_FOUND(HttpStatus.NOT_FOUND, "멘토링을 찾을 수 없습니다."),
-	NOT_YOUR_APPLY(HttpStatus.FORBIDDEN, "본인이 신청한 멘토링만 취소할 수 있습니다.");
+	NOT_YOUR_APPLY(HttpStatus.FORBIDDEN, "본인이 신청한 멘토링만 취소할 수 있습니다."),
+	CAN_NOT_APPLY_YOUR_CLASS(HttpStatus.FORBIDDEN, "자신의 멘토링 클래스에 신청할 수 없습니다.");
 
 
 	private final HttpStatus status;
