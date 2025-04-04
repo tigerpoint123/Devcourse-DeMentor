a26ff1c644a906d2d72fc48ff3e26e3b9dc6333e
Daewook123 <cdo5080@gmail.com>
2025-04-04 11:48:32 +0900

    remove

--- src/main/java/com/dementor/DeMentorApplication.java
+++ src/main/java/com/dementor/DeMentorApplication.java
@@ -2,9 +2,7 @@ package com.dementor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
 
-@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 @SpringBootApplication
 public class DeMentorApplication {
 	// 개발 세팅 중
