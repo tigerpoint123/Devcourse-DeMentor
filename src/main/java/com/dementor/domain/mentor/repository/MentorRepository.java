2a84ed8f24f4f25a684cbf6049d05d8bba43f8de
baekminjin <bhs3392@naver.com>
2025-04-04 11:06:28 +0900

    feat: 신청한 멘티 혹은 멘토 목록 조회
    멘토가 자신의 멘토링 신청한 멘티 목록을 조회하기 위한 API 엔드포인트

--- src/main/java/com/dementor/domain/mentor/repository/MentorRepository.java
+++ src/main/java/com/dementor/domain/mentor/repository/MentorRepository.java
@@ -1,7 +1,13 @@
 package com.dementor.domain.mentor.repository;
 
+import java.util.List;
+
 import com.dementor.domain.mentor.entity.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MentorRepository extends JpaRepository<Mentor, Long> {
+	@Query("SELECT mc.id FROM MentoringClass mc WHERE mc.mentor = :mentor")
+	List<Long> findMentoringClassIdsByMentor(@Param("mentor") Mentor mentor);
 }
