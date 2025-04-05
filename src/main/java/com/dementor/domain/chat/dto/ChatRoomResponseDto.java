13153b3bbcde6866ecc4a279826979de6334a3bc
pingu0118 <rnjsqhrud99@naver.com>
2025-04-05 15:08:36 +0900

    feat: 닉네임 변환 & ApplyResponse mentor/mentee Id 반환

--- src/main/java/com/dementor/domain/chat/dto/ChatRoomResponseDto.java
+++ src/main/java/com/dementor/domain/chat/dto/ChatRoomResponseDto.java
@@ -15,9 +15,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class ChatRoomResponseDto {
     private Long chatRoomId;
-//    private Long applymentId;  //null이면 관리자 채팅방
     private RoomType roomType;     // "MENTORING_CHAT" or "ADMIN_CHAT"
-    private String nickname;  // 멘토,멘티의 닉네임 & 관리자닉네임 = 관리자
     private String lastMessage;
     private LocalDateTime lastSentAt;
     private String targetNickname;  //상대방 닉네임
