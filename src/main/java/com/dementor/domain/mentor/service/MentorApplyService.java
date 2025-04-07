26722da690a1f150afd42aad4fc56306c041677d
baekminjin <bhs3392@naver.com>
2025-04-07 11:40:33 +0900

    feat: 멘토링 신청 승인/거절 서비스

--- src/main/java/com/dementor/domain/mentor/service/MentorApplyService.java
+++ src/main/java/com/dementor/domain/mentor/service/MentorApplyService.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dementor.domain.apply.entity.Apply;
+import com.dementor.domain.apply.entity.ApplyStatus;
 import com.dementor.domain.apply.repository.ApplyRepository;
+import com.dementor.domain.mentor.dto.request.ApplyStatusRequest;
+import com.dementor.domain.mentor.dto.response.ApplyStatusResponse;
 import com.dementor.domain.mentor.dto.response.MentorApplyResponse;
 import com.dementor.domain.mentor.entity.Mentor;
 import com.dementor.domain.mentor.repository.MentorRepository;
@@ -26,7 +29,7 @@ public class MentorApplyService {
 	private final ApplyRepository applyRepository;
 	private final MentorRepository mentorRepository;
 
-	@Transactional
+
 	public MentorApplyResponse.GetApplyMenteePageList getApplyByMentor(Long memberId, int page, int size) {
 
 		Mentor mentor = mentorRepository.findById(memberId)
@@ -44,6 +47,41 @@ public class MentorApplyService {
 		Page<Apply> applyPage = applyRepository.findByMentoringClassIdIn(classId, PageRequest.of(page, size));
 
 		return MentorApplyResponse.GetApplyMenteePageList.from(applyPage, page, size);
+	}
 
+
+	@Transactional
+	public ApplyStatusResponse updateApplyStatus(Long memberId, Long applyId, ApplyStatusRequest request) {
+		// 멘토 검증
+		Mentor mentor = mentorRepository.findById(memberId)
+			.orElseThrow(() -> new IllegalArgumentException("멘토만 상태를 변경할 수 있습니다."));
+			
+		if (mentor.getApprovalStatus() != Mentor.ApprovalStatus.APPROVED) {
+			throw new AccessDeniedException("승인되지 않은 멘토는 신청 상태를 변경할 수 없습니다");
+		}
+		
+		// 신청 정보 조회
+		Apply apply = applyRepository.findById(applyId)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 신청입니다."));
+			
+		// 멘토의 클래스인지 확인
+		List<Long> classIds = mentorRepository.findMentoringClassIdsByMentor(mentor);
+		if (!classIds.contains(apply.getMentoringClass().getId())) {
+			throw new AccessDeniedException("자신의 멘토링 클래스에 대한 신청만 변경할 수 있습니다.");
+		}
+
+		//이미 승인/거절을 한 신청인지 확인
+		if (apply.getApplyStatus() == ApplyStatus.APPROVED || apply.getApplyStatus() == ApplyStatus.REJECTED) {
+			throw new IllegalArgumentException("이미 승인/거절된 신청입니다.");
+		}
+
+		// 상태 변경
+		ApplyStatus newStatus = request.getStatus();
+		apply.updateStatus(newStatus);
+		
+		// 변경된 엔티티 저장
+		Apply updatedApply = applyRepository.save(apply);
+		
+		return ApplyStatusResponse.from(updatedApply);
 	}
 }
