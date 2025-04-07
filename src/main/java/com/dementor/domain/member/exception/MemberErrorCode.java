2783b9f7e1aa03f0f750d3dd7ebb87d728f8dbd4
Daewook123 <cdo5080@gmail.com>
2025-04-07 16:40:35 +0900

    cors 오류 수정중

--- src/main/java/com/dementor/domain/member/exception/MemberErrorCode.java
+++ src/main/java/com/dementor/domain/member/exception/MemberErrorCode.java
@@ -14,6 +14,7 @@ public enum MemberErrorCode  {
     INVALID_VERIFYCODE(HttpStatus.BAD_REQUEST, "인증번호가 유효하지 않습니다"),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다"),
     INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "올바르지 않은 인증번호입니다");
+
     
     private final HttpStatus status;
     private final String message;
