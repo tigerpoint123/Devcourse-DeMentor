7aa0485e20306ec79baa01802ee245f93efcc405
pingu0118 <rnjsqhrud99@naver.com>
2025-04-09 16:15:08 +0900

    refactor: 헤더포함-> 쿠키ë. 필요없는 파일 주석처리

--- src/main/java/com/dementor/global/websocket/JwtHandshakeInterceptor.java
+++ src/main/java/com/dementor/global/websocket/JwtHandshakeInterceptor.java
@@ -1,37 +1,37 @@
-package com.dementor.global.websocket;
-
-
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-
-import java.util.Map;
-
-public class JwtHandshakeInterceptor implements HandshakeInterceptor {
-
-    @Override
-    public boolean beforeHandshake(
-            ServerHttpRequest request, ServerHttpResponse response,
-            WebSocketHandler wsHandler, Map<String, Object> attributes) {
-
-        if (request instanceof ServletServerHttpRequest servletRequest) {
-            HttpServletRequest httpRequest = servletRequest.getServletRequest();
-            String token = httpRequest.getHeader("Authorization");
-
-            if (token != null && token.startsWith("Bearer ")) {
-                attributes.put("jwt", token.substring(7)); // "Bearer " 제외
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public void afterHandshake(ServerHttpRequest request,
-                               ServerHttpResponse response,
-                               WebSocketHandler wsHandler,
-                               Exception ex) {
-    }
-}
+//package com.dementor.global.websocket;
+//
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.http.server.ServletServerHttpRequest;
+//import org.springframework.http.server.ServerHttpRequest;
+//import org.springframework.http.server.ServerHttpResponse;
+//import org.springframework.web.socket.WebSocketHandler;
+//import org.springframework.web.socket.server.HandshakeInterceptor;
+//
+//import java.util.Map;
+//
+//public class JwtHandshakeInterceptor implements HandshakeInterceptor {
+//
+//    @Override
+//    public boolean beforeHandshake(
+//            ServerHttpRequest request, ServerHttpResponse response,
+//            WebSocketHandler wsHandler, Map<String, Object> attributes) {
+//
+//        if (request instanceof ServletServerHttpRequest servletRequest) {
+//            HttpServletRequest httpRequest = servletRequest.getServletRequest();
+//            String token = httpRequest.getHeader("Authorization");
+//
+//            if (token != null && token.startsWith("Bearer ")) {
+//                attributes.put("jwt", token.substring(7)); // "Bearer " 제외
+//            }
+//        }
+//        return true;
+//    }
+//
+//    @Override
+//    public void afterHandshake(ServerHttpRequest request,
+//                               ServerHttpResponse response,
+//                               WebSocketHandler wsHandler,
+//                               Exception ex) {
+//    }
+//}
