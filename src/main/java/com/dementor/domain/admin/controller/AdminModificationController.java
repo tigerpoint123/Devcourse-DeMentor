33e3dd5c5358709801faab850facf598a62d2e36
Honam Kim <tigerrla@naver.com>
2025-04-10 14:39:05 +0900

    feat : 멘토 정보 수정 요청 조회, 승인거절 구현

--- src/main/java/com/dementor/domain/admin/controller/AdminModificationController.java
+++ src/main/java/com/dementor/domain/admin/controller/AdminModificationController.java
@@ -1,30 +1,25 @@
 package com.dementor.domain.admin.controller;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.dementor.domain.mentor.dto.response.MentorChangeResponse;
 import com.dementor.domain.mentoreditproposal.dto.MentorEditFindAllRenewalResponse;
 import com.dementor.domain.mentoreditproposal.dto.MentorEditUpdateRenewalResponse;
 import com.dementor.domain.mentoreditproposal.service.AdminModificationService;
 import com.dementor.global.ApiResponse;
 import com.dementor.global.common.pagination.PaginationUtil;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admain/mentor")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminModificationController {
 
-	private AdminModificationService adminModificationService;
+	private final AdminModificationService adminModificationService;
 
 	@GetMapping("/modifyList")
 	//@PreAuthorize("hasRole('ADMIN')") / 수정 요청 목록 전체조회
@@ -58,6 +53,22 @@ public class AdminModificationController {
 				"수정 성공",
 				response
 			));
+	}
+
+	@PutMapping("/modify/{memberId}/reject")
+	public ResponseEntity<ApiResponse<MentorEditUpdateRenewalResponse>> rejectMentorUpdate(
+			@PathVariable Long memberId
+	) {
+		MentorEditUpdateRenewalResponse response = adminModificationService.rejectMentorUpdate(memberId);
+
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(ApiResponse.of(
+						true,
+						HttpStatus.OK,
+						"수정 성공",
+						response
+				));
 	}
 
 }
