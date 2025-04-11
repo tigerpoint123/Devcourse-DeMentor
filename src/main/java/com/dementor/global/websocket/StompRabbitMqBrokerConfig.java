b4859cde8f38b81381d5fecebd589977fe6dc812
Daewook123 <cdo5080@gmail.com>
2025-04-11 15:38:54 +0900

    CI/CD 설정 수정 env설정

--- src/main/java/com/dementor/global/websocket/StompRabbitMqBrokerConfig.java
+++ src/main/java/com/dementor/global/websocket/StompRabbitMqBrokerConfig.java
@@ -2,6 +2,7 @@ package com.dementor.global.websocket;
 
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -13,6 +14,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class StompRabbitMqBrokerConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitmqHost;
+
+    @Value("${spring.rabbitmq.username}")
+    private String rabbitmqUsername;
+
+    @Value("${spring.rabbitmq.password}")
+    private String rabbitmqPassword;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").withSockJS();
@@ -23,12 +33,12 @@ public class StompRabbitMqBrokerConfig implements WebSocketMessageBrokerConfigur
         registry
                 .setApplicationDestinationPrefixes("/app")
                 .enableStompBrokerRelay("/topic")
-                .setRelayHost("localhost")
+                .setRelayHost(rabbitmqHost)
                 .setRelayPort(61613)
-                .setClientLogin("admin")
-                .setClientPasscode("admin")
-                .setSystemLogin("admin")
-                .setSystemPasscode("admin");
+                .setClientLogin(rabbitmqUsername)
+                .setClientPasscode(rabbitmqPassword)
+                .setSystemLogin(rabbitmqUsername)
+                .setSystemPasscode(rabbitmqPassword);
     }
 
     @Bean
